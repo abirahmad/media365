5219c09eeced3fa6e1839b9072755241fe4ce8f9
Abir Ahmad <abir.ahmad007@gmail.com>
2025-08-25 16:56:36 +0600

    first push

---
+++ database/migrations/2025_08_25_105322_create_thumbnail_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('thumbnail_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['pending', 'processing', 'completed', 'failed'])->default('pending');
+            $table->integer('total_images');
+            $table->integer('processed_images')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('thumbnail_requests');
+    }
+};
